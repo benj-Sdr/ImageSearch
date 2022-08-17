46482584310841a9ff689c0898e72e7143a5293e
Biniam Tefrie <103905653+benj-Sdr@users.noreply.github.com>
2022-08-17 19:02:57 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,43 @@
+import { useState, useEffect} from "react"
+import axios from 'axios'
+import './style.css'
+const App = () => {
 
+const [search, setSearch] = useState([])
+
+const [show, setShow] = useState([])
+console.log(show)
+  const inputSearch = (event) => {
+        setSearch(event.target.value)
+  }
+  
+useEffect(() => {
+  axios
+        .get(`https://api.unsplash.com/search/photos?client_id=x8SAb07qlzJEkXO0QhY7Q8LpCFTOiyFA3xi1e-IJ0FI&query=${search}`)
+        .then(res => {
+          const mapResult = res.data.results.map(item => {
+            console.log(item)
+            return <img src={item.urls.small_s3} alt={item.alt_description}/>
+          })
+          setShow(mapResult)
+        })
+}, [search])
+
+   
+
+  return(
+    <div className="app">
+      <div className="img-search">
+          <label className="label">Image Search</label><br/>
+          <input onChange={inputSearch}/>
+      </div>
+       
+        { 
+          <div className="show">
+             {show}
+          </div>
+        }
+    </div>
+  )
+}
+export default App
